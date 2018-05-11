7f8ec6c349e23a415a491d5b2ed0e1dde4ff55c8
Tomasz Suchwałko <tomasz.suchwalko@gmail.com>
2018-05-11 16:09:56 +0200

    Add project files.

---
+++ FinalBattle/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FinalBattle.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
