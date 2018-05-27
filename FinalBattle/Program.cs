5d1681e2dde6e9cdc3d4d765a1c72f5cae4282f5
Tomasz Suchwałko <tomasz.suchwalko@gmail.com>
2018-05-28 01:23:08 +0200

    https

--- FinalBattle/Program.cs
+++ FinalBattle/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -18,14 +19,24 @@ namespace FinalBattle
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
-                        WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-                //      WebHost.CreateDefaultBuilder(args)
-                //.UseKestrel()
-                //.UseUrls("http://*:80")
-                //.UseContentRoot(Directory.GetCurrentDirectory())
-                //              .UseStartup<Startup>()
-                //              .Build();
+            new WebHostBuilder()
+            .UseKestrel(options =>
+            {
+                options.Listen(IPAddress.Any, 443, listenOptions =>
+                {
+                    listenOptions.UseHttps("server.pfx", "password");
+                });
+            })
+            .UseStartup<Startup>()
+            .Build();
+        //WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //        .Build();
+        //      WebHost.CreateDefaultBuilder(args)
+        //.UseKestrel()
+        //.UseUrls("http://*:80")
+        //.UseContentRoot(Directory.GetCurrentDirectory())
+        //              .UseStartup<Startup>()
+        //              .Build();
     }
 }
